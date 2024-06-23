8aa2e7796bc4d6afee3e528daa1b711dd9e2bb32
mawethuN <2128875@students.wits.ac.za>
2024-06-23 23:12:28 +0200

    remove folders

--- navbar.js
+++ navbar.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
         <nav id="navbar">
             <ul>
                 <li><a href="index.html">Home</a></li>
-                <li><a href="blog/blog.html">Blogs</a></li>
+                <li><a href="blog.html">Blogs</a></li>
                 <li><a href="design.html">Design</a></li>
-                <li><a href="essay/essay.html">Essay</a></li>
-                <li><a href="profile/Profile.html">Profile</a></li>
+                <li><a href="essay.html">Essay</a></li>
+                <li><a href="Profile.html">Profile</a></li>
             </ul>
         </nav>
     `;
