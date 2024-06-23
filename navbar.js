de40222a7860dd67cb9fbd45b8e0d0aee4a8d0b2
mawethuN <2128875@students.wits.ac.za>
2024-06-23 23:06:07 +0200

    .

--- navbar.js
+++ navbar.js
@@ -1,16 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const navbarHTML = `
         <nav id="navbar">
             <ul>
-                <li><a href="/index.html">Home</a></li>
-                <li><a href="/blog/blog.html">Blogs</a></li>
-                <li><a href="/design.html">Design</a></li>
-                <li><a href="/essay/essay.html">Essay</a></li>
-                <li><a href="/profile/Profile.html">Profile</a></li>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="blog/blog.html">Blogs</a></li>
+                <li><a href="design.html">Design</a></li>
+                <li><a href="essay/essay.html">Essay</a></li>
+                <li><a href="profile/Profile.html">Profile</a></li>
             </ul>
         </nav>
     `;
 
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
+
